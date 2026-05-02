47ba1a497b5dfd8be25e6d14158cf9ec4e4dfa61
ksiki <j.kolyasha@gmail.com>
2026-05-02 20:54:01 +0300

    app orchestrator

---
+++ api-service/src/core/app_orchestrator.py
@@ -0,0 +1,34 @@
+import logging
+
+from core.config import settings
+from db.core.db_postgre import database as db_pg
+from db.core.db_s3 import database as db_s3
+from db.scripts.db_seeder import DatabaseSeeder
+from db.scripts.infrastructure_init import InfrastructureInitializer
+
+logger = logging.getLogger(__name__)
+
+
+class AppOrchestrator:
+    def __init__(self):
+        pass
+
+    async def setup(self) -> None:
+        logger.info("Begin setup app.")
+
+        async with db_s3.session_dependency() as s3_session:
+            await InfrastructureInitializer.run_bootstrap(
+                s3_session=s3_session, bucket_name=settings.s3_bucket_name
+            )
+        async with (
+            db_pg.session_dependency() as pg_session,
+            db_s3.session_dependency() as s3_session,
+        ):
+            await DatabaseSeeder.run_bootstrap(
+                pg_session=pg_session, s3_session=s3_session
+            )
+
+        logger.info("App setup completed successfully.")
+
+    async def shutdown(self) -> None:
+        await db_pg.dispose()

b37d590335519bc1c7698dc6894d09490ba436a7
ksiki <j.kolyasha@gmail.com>
2026-05-08 13:08:02 +0300

    worker-consumer task handlers

---
+++ worker-consumer/worker/handlers/posts.py
@@ -0,0 +1,78 @@
+import logging
+
+from better_profanity import profanity
+from config import settings
+from sightengine.client import SightengineClient
+
+from shared.db.repositories.images import ImageRepository
+from shared.db.repositories.posts import PostRepository
+from shared.db.repositories.users import UserRepository
+from shared.enums.image_category import ImageCategory
+from shared.queue.tasks import notify_user
+
+logger = logging.getLogger(__name__)
+_sightengine_client = SightengineClient(
+    settings.sightengine_api_user, settings.sightengine_api_secret
+)
+profanity.load_censor_words()
+
+
+class PostHandler:
+    @staticmethod
+    async def archive_posts(post_rep: PostRepository) -> None:
+        await post_rep.archivate_all_old_posts()
+
+    @staticmethod
+    async def delete_posts(post_rep: PostRepository, img_rep: ImageRepository) -> None:
+        images = await post_rep.delete_all_old_posts()
+        for img in images:
+            await img_rep.delete(category=ImageCategory.POST, name=img)
+
+    @staticmethod
+    async def validate_posts(
+        post_id: int,
+        post_rep: PostRepository,
+        user_rep: UserRepository,
+        image_rep: ImageRepository,
+    ) -> None:
+        post = await post_rep.get_by_id(id=post_id)
+        if not post:
+            return
+
+        async def notify_on_invalid(message: str) -> None:
+            user = await user_rep.get_by_id(id=post.user_id)
+            text = f"Ваш пост #{post.id} был удален. Причина: {message}"
+            await post_rep.delete_by_id(id=post.id)
+            await notify_user.kiq(tg_id=user.tg_id, message=text)
+
+        if profanity.contains_profanity(post.description):
+            await notify_on_invalid(
+                message="В описании продукта содержится ненормативная лексика"
+            )
+            return
+
+        try:
+            image_url = await image_rep.get_url(
+                category=ImageCategory.POST, name=post.image_name
+            )
+            output = _sightengine_client.check(
+                "nudity", "wad", "offensive", "gore"
+            ).set_url(image_url)
+
+            if output.get("status") == "success":
+                if output["gore"]["prob"] > 0.5:
+                    await notify_on_invalid(message="На фото обнаружена жесть/кровь")
+                    return
+                if output["nudity"]["raw"] > 0.5:
+                    await notify_on_invalid(
+                        message="На фото обнаружен неприемлемый контент"
+                    )
+                    return
+
+                lifetime = post.disable_at - post.pub_at
+                await post_rep.reactivate(post_id=post.id, lifetime=lifetime)
+        except Exception as e:
+            logger.error(f"Ошибка валидации изображения: {e}")
+            await notify_on_invalid(
+                message="Приносим свои извинения - у нас возникли проблемы с проверкой вашего поста. Попробуйте позже или обратитесь в поддержку"
+            )
---
+++ worker-consumer/worker/handlers/user_subscriptions.py
@@ -0,0 +1,25 @@
+import logging
+from datetime import timedelta
+
+from shared.db.repositories.subscriptions import SubscriptionRepository
+from shared.db.repositories.user_subscriptions import UserSubscriptionRepository
+
+logger = logging.getLogger(__name__)
+
+
+class UserSubscriptionHandler:
+    @staticmethod
+    async def renew_expired_subscriptions_with_free(
+        user_subs_rep: UserSubscriptionRepository, sub_rep: SubscriptionRepository
+    ) -> None:
+        expired_user_ids = await user_subs_rep.deactivate_expired_subscriptions()
+
+        if not expired_user_ids:
+            logger.info("No expired subscriptions found.")
+            return
+
+        free_sub = await sub_rep.get_free_subscription()
+
+        await user_subs_rep.bulk_subscribe_free(
+            expired_user_ids, free_sub.id, timedelta(free_sub.duration_days)
+        )

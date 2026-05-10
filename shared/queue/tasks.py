12e81b00b0675d10a0a2ebaf5e293a60377f4974
ksiki <j.kolyasha@gmail.com>
2026-05-10 12:32:05 +0300

    update tsks

--- shared/queue/tasks.py
+++ shared/queue/tasks.py
@@ -7,7 +7,9 @@ from shared.queue.broker import broker
     schedule=[{"cron": "0 */2 * * *"}],
 )
 async def archive_posts_task() -> None:
-    pass
+    from worker.tasks.posts import archive_posts_task
+
+    await archive_posts_task()
 
 
 @broker.task(
@@ -15,7 +17,9 @@ async def archive_posts_task() -> None:
     schedule=[{"cron": "40 */2 * * *"}],
 )
 async def delete_posts_task() -> None:
-    pass
+    from worker.tasks.posts import delete_posts_task
+
+    await delete_posts_task()
 
 
 @broker.task(
@@ -23,14 +27,20 @@ async def delete_posts_task() -> None:
     schedule=[{"cron": "* 0 * * *"}],
 )
 async def checking_user_subscriptions_task() -> None:
-    pass
+    from worker.tasks.users import checking_user_subscriptions_task
+
+    await checking_user_subscriptions_task()
 
 
 @broker.task(task_name=TaskType.VALIDATE_POST)
 async def validate_post_task(post_id: int) -> None:
-    pass
+    from worker.tasks.posts import validate_post_task
+
+    await validate_post_task(post_id=post_id)
 
 
 @broker.task(task_name=TaskType.NOTIFY_USER)
 async def notify_user(tg_id: int, message: str) -> None:
-    pass
+    from bot.task_handler import send_notify
+
+    await send_notify(tg_id=tg_id, message=message)
